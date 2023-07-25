66687506e7cb444e955a314676c89eb2af4bcabb
SamAska <117988639+SamAska@users.noreply.github.com>
2023-07-25 23:24:38 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,10 +1,11 @@
 #include "main.h"
 void print_buffer(char buffer[], int *buff_ind);
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - This is a command Printf function
+ * @format: the format for the printf.
+ * Return: this command shows Printed chars.
  */
+
 int _printf(const char *format, ...)
 
 {
@@ -90,9 +91,9 @@ return (printed_chars);
 
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * print_buffer - this command Prints the contents of the buffer if it exist
+ * @buffer: this shows the Array of chars
+ * @buff_ind: This is the Index at which to add next char, represents the length.
  */
 
 void print_buffer(char buffer[], int *buff_ind)
